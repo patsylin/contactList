bb9272565a3ab4c822342187287e345d852ac7f7
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-08-08 15:54:13 -0500

    updates from class

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import ContactList from './components/ContactList';
 import './App.css'
+import { useState } from 'react'
+import ContactList from "./components/ContactList"
+import SelectedContact from './components/SelectedContact'
 
 export default function App() {
-    return (
-      <>
-        <ContactList />
-      </>
-    );
-  }
+  const [selectedContactId, setSelectedContactId] = useState(null)
+
+  return (
+    <>
+      { selectedContactId ? (
+        <SelectedContact
+          myId={selectedContactId}
+          pickMyId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList chosenId={setSelectedContactId}/>
+      )}
+
+    </>
+  )
+}
+
+  //id which contact to render
+  //a new component
+  //fetching a single id
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,11 +1,13 @@
-//import { useState } from 'react'
-
-export default function ContactRow({ bubbles }) {
-    return (
-      <tr>
-        <td>{bubbles.name}</td>
-        <td>{bubbles.email}</td>
-        <td>{bubbles.phone}</td>
+export default function ContactRow({ bubbles, pickId }) {
+  return (
+      <tr
+          onClick={() => {
+              pickId(bubbles.id)
+          }}
+      >
+          <td>{bubbles.name}</td>
+          <td>{bubbles.email}</td>
+          <td>{bubbles.phone}</td>
       </tr>
-    );
-  }
+  )
+}
